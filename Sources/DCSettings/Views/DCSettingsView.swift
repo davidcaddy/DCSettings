aa3ec1669c3c4c12faa8df1af2c4d823b06296c7
David Caddy <dev@davidcaddy.com>
2023-05-30 00:08:27 +1000

    Simplify DCSettingsView use with filter replacing function of 'includeSettingsWithoutLabels' and 'hiddenKeys' parameters

--- Sources/DCSettings/Views/DCSettingsView.swift
+++ Sources/DCSettings/Views/DCSettingsView.swift
@@ -8,48 +8,71 @@ import SwiftUI
 
 /// A view that displays a list of grouped settings.
 ///
-/// `DCSettingsView` is a view that displays a list of grouped settings. The view takes a `DCSettingsManager` instance as an argument and displays a list of all settings managed by that instance.
+/// `DCSettingsView` is a view that displays a list of grouped settings.
+/// The view takes a `DCSettingsManager` instance as an argument and displays a list of all settings managed by that instance.
 ///
 /// The view uses a `DCSettingViewProviding` instance to provide custom views for individual settings.
 /// If no custom view is available for a specific setting, a default view will be used if the setting's value is a supported type.
-/// Supported types as standard are: `Bool`, `Int`,  `Double`, `String`, `Date` and `Color`.
+/// Supported types as standard are: `Bool`, `Int`, `Double`, `String`, `Date` and `Color`.
 @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 9.0, *)
-public struct DCSettingsView<Provider: DCSettingViewProviding>: View {
+public struct DCSettingsView<Provider: DCSettingViewProviding, S: ListStyle>: View {
+    
+    /// An enumeration that defines the available filters for the settings view.
+    public enum Filter: Equatable {
+        
+        /// A filter that includes only settings with labels.
+        case labelled
+        
+        /// A filter that excludes settings with the specified keys.
+        case excludeKeys([String])
+    }
     
     private let settingsManager: DCSettingsManager
-    
-    private var includeSettingsWithoutLabels: Bool
-    
-    private var hiddenKeys: [String]
-    
+    private let filter: Filter?
     private let contentProvider: Provider?
+    private var listStyle: S
+    
+    private var hiddenKeys: [String] {
+        if case .excludeKeys(let keys) = filter {
+            return keys
+        }
+        return []
+    }
 
-    /// Initializes a new `DCSettingsView` instance with the specified settings manager, inclusion flag, hidden keys, and content provider.
+    /// Initializes a new `DCSettingsView` instance with the specified settings manager, filter, and content provider.
     ///
-    /// This initializer creates a new instance of `DCSettingsView` with the specified settings manager, inclusion flag, hidden keys, and content provider.
-    /// The settings manager is required, while the inclusion flag, hidden keys, and content provider are optional.
+    /// This initializer creates a new instance of `DCSettingsView` with the specified settings manager, filter, and content provider.
+    /// The settings manager is required, while the filter and content provider are optional.
     ///
     /// - Parameters:
     ///   - settingsManager: A `DCSettingsManager` instance used to manage the settings.
     ///   The default value is the `.shared` singleton instance.
-    ///   - includeSettingsWithoutLabels: A boolean value indicating whether to include settings without labels in the list.
-    ///   The default value is `false`.
-    ///   - hiddenKeys: An array of keys representing settings that should be hidden from the list. The default value is an empty array.
+    ///   - filter: A `Filter` value used to filter the displayed settings. The default value is `nil`.
     ///   - contentProvider: A `DCSettingViewProviding` instance used to provide custom views for individual settings.
     ///   The default value is an instance of `DCDefaultViewProvider`.
-    public init(settingsManager: DCSettingsManager = .shared, includeSettingsWithoutLabels: Bool = false, hiddenKeys: [DCKeyRepresentable] = [], contentProvider: Provider? = DCDefaultViewProvider()) {
-        self.settingsManager = settingsManager
-        self.includeSettingsWithoutLabels = includeSettingsWithoutLabels
-        self.hiddenKeys = hiddenKeys.map({ $0.keyValue })
-        self.contentProvider = contentProvider
-    }
+    ///   - listStyle: A `ListStyle` value used to set the style of the list. The default value is platform-dependent.
+    #if os(macOS)
+        public init(settingsManager: DCSettingsManager = .shared, filter: Filter? = nil, contentProvider: Provider? = DCDefaultViewProvider(), listStyle: Style = SidebarListStyle()) {
+            self.settingsManager = settingsManager
+            self.filter = filter
+            self.contentProvider = contentProvider
+            self.listStyle = listStyle
+        }
+    #elseif os(iOS)
+        public init(settingsManager: DCSettingsManager = .shared, filter: Filter? = nil, contentProvider: Provider? = DCDefaultViewProvider(), listStyle: S = InsetGroupedListStyle()) {
+            self.settingsManager = settingsManager
+            self.filter = filter
+            self.contentProvider = contentProvider
+            self.listStyle = listStyle
+        }
+    #endif
     
     public var body: some View {
         List(settingsManager.groups) { group in
             if !hiddenKeys.contains(group.key.keyValue) {
                 Section(group.label?.sentenceFormatted ?? "") {
                     ForEach(group.settings, id: \.key) { setting in
-                        if ((setting.label != nil) || includeSettingsWithoutLabels) && !hiddenKeys.contains(setting.key.keyValue) {
+                        if ((setting.label != nil) || (filter != .labelled)) && !hiddenKeys.contains(setting.key.keyValue) {
                             if let content = contentProvider?.content(for: setting), !(content is EmptyView) {
                                 content
                             }
@@ -61,11 +84,7 @@ public struct DCSettingsView<Provider: DCSettingViewProviding>: View {
                 }
             }
         }
-        #if os(macOS)
-        .listStyle(SidebarListStyle())
-        #elseif os(iOS)
-        .listStyle(InsetGroupedListStyle())
-        #endif
+        .listStyle(listStyle)
     }
 }
 
