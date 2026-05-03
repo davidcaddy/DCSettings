6b46e893f748172564249b18640bd4493ca236d2
David Caddy <dev@davidcaddy.com>
2026-05-03 16:13:42 +1000

    Make Color Codable @retroactive on Swift 6+
    Use conditional compilation to declare Color conformance to Codable as @retroactive when building with Swift 6+ (compiler>=6.0), while keeping a normal extension for older compilers. Preserves the platform availability (macOS 11 / iOS 14 / tvOS 14 / watchOS 7) and moves the implementation into a separate availability-marked extension. This ensures retroactive conformance is applied where supported without breaking older toolchains.

--- Sources/DCSettings/Extensions/Color+Codable.swift
+++ Sources/DCSettings/Extensions/Color+Codable.swift
@@ -13,8 +13,16 @@ import UIKit
 import AppKit
 #endif
 
+#if compiler(>=6.0)
 @available(macOS 11.0, iOS 14.0, tvOS 14.0, watchOS 7.0, *)
-extension Color: Codable {
+extension Color: @retroactive Codable {}
+#else
+@available(macOS 11.0, iOS 14.0, tvOS 14.0, watchOS 7.0, *)
+extension Color: Codable {}
+#endif
+
+@available(macOS 11.0, iOS 14.0, tvOS 14.0, watchOS 7.0, *)
+extension Color {
     
     #if canImport(UIKit)
     typealias NativeColor = UIColor
